e9abc5b4e4b813992e2c69ed47f30a5c85e011be
phatthana <phatthana@me.com>
2015-07-10 19:23:48 +0700

    change default port

--- benchmark.js
+++ benchmark.js
@@ -7,7 +7,7 @@ function user(shouldBroadcast, host, port) {
   var socket = io.connect('http://' + host + ':' + port, {'force new connection': true});
 
   socket.on('connect', function() {
-    
+
     // Start messaging loop
     if (shouldBroadcast) {
       // message will be broadcasted by server
@@ -18,7 +18,7 @@ function user(shouldBroadcast, host, port) {
     }
 
     socket.on('message', function(message) {
-      socket.send(message);    
+      socket.send(message);
     });
 
     socket.on('broadcastOk', function() {
@@ -34,7 +34,7 @@ var rampUpTime = parseInt(process.argv[argvIndex++]) * 1000; // in seconds
 var newUserTimeout = rampUpTime / users;
 var shouldBroadcast = process.argv[argvIndex++] === 'broadcast' ? true : false;
 var host = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : 'localhost';
-var port = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '3000';
+var port = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '1234';
 
 for(var i=0; i<users; i++) {
   setTimeout(function() { user(shouldBroadcast, host, port); }, i * newUserTimeout);
