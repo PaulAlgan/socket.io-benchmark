731f650e2487d39f15d63263e8b7bb2b9cb9753e
phatthana <phatthana@me.com>
2015-07-29 15:04:46 +0700

    commit

--- benchmark.js
+++ benchmark.js
@@ -13,12 +13,16 @@ function user(shouldBroadcast, host, port) {
   var socket = io.connect('http://' + host + ':' + port, options);
   console.log("Opening SOCKET");
   socket.on('connect', function() {
+
     connectionCount++;
     console.log("   SOCKET connect "+connectionCount);
+    sendMsg(socket);
+
     // var chat = {text:"TEXT_"};
     // setInterval(function() {
     //   socket.emit("chat", chat);
-    // }, 5000);
+    // }, 2000);
+
     // Start messaging loop
     // if (shouldBroadcast) {
     //   // message will be broadcasted by server
@@ -28,9 +32,12 @@ function user(shouldBroadcast, host, port) {
     //   socket.send(message);
     // }
 
-    // socket.on('chat', function(message) {
-    //   socket.send(message);
-    // });
+    socket.on('chat', function(message) {
+      console.log(socket.id+" GET MSG: "+message.text);
+      setTimeout(function(){
+        sendMsg(socket);
+      },5000);
+    });
 
     socket.on('broadcastOk', function() {
       socket.emit('broadcast', message);
@@ -42,6 +49,13 @@ function user(shouldBroadcast, host, port) {
     // console.log(data);
     console.log(data.description.target._events);
   });
+
+  function sendMsg(socket){
+
+    var chat = {text:"TEXT_"+socket.id};
+    console.log("SEND MSG"+chat.text);
+    socket.emit("chat", chat);
+  }
 };
 
 var argvIndex = 2;
@@ -50,8 +64,9 @@ var users = parseInt(process.argv[argvIndex++]);
 var rampUpTime = parseInt(process.argv[argvIndex++]) * 1000; // in seconds
 var newUserTimeout = rampUpTime / users;
 var shouldBroadcast = process.argv[argvIndex++] === 'broadcast' ? true : false;
-var host = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '54.251.48.117';
-var port = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '80';
+var host = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '128.199.244.231';
+// var host = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : 'localhost';
+var port = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '1234';
 
 for(var i=0; i<users; i++) {
   setTimeout(function() { user(shouldBroadcast, host, port); }, i * newUserTimeout);
